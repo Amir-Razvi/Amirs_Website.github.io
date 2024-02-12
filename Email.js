ab8ded9711a45cd5732228fd69f0933c7a0b7cc1
Amir-Razvi <67238892+Amir-Razvi@users.noreply.github.com>
2024-02-12 01:14:06 -0500

    Add files via upload
    Initial Commit

---
+++ Email.js
@@ -0,0 +1,3 @@
+    <><script src="https://cdn.emailjs.com/dist/email.min.js"></script><script>
+        emailjs.init("baseerahmedrazvi@gmail.com"); // Replace with your Email.js user ID
+    </script></>
